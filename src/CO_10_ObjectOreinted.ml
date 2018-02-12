d01c2f1aa1c559e22771aa5dc2d7a7a92acb1ef9
François Berhn <baudetad@gmail.com>
2018-02-12 09:42:51 +0100

    Samll fix

--- src/CO_10_ObjectOreinted.ml
+++ src/CO_10_ObjectOreinted.ml
@@ -1,6 +1,6 @@
 let compteur maxi =
   object(self)
-  val mutable max = maxi
+  val max = maxi
   method say x = if x <= maxi
     then let _ = print_endline (string_of_int x) in self#say (x+1)
   method count = self#say 1
