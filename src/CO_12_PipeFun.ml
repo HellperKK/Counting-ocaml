0c08476bdba688f0901c06e5cb159f68a3032452
François Berhn <baudetad@gmail.com>
2018-03-27 09:21:09 +0200

    added version

---
+++ src/CO_12_PipeFun.ml
@@ -0,0 +1,8 @@
+let print_int_bis x = let _ = x |> string_of_int |> print_endline in x
+
+let count_piped max =
+  let rec aux x = if x <= max
+    then x |> print_int_bis |> ((+) 1) |> aux
+  in aux 1
+
+let counter_twelve = count_piped 10
