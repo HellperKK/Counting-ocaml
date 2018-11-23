3d10fed18c1c43d16b4600268d8702be3c480543
Adrien Baudet <baudetad@gmail.com>
2018-11-23 10:20:15 +0100

    simplified example 1

--- src/CO_01_SimpleFunction.ml
+++ src/CO_01_SimpleFunction.ml
@@ -1,8 +1,7 @@
 (* Simple function *)
 let count_upto max =
-	let rec aux = function
-		|x when x > max -> ()
-		|x -> let _ = print_endline (string_of_int x) in aux (x + 1)
+	let rec aux x = if x <= max
+		then let _ = print_endline (string_of_int x) in aux (x + 1)
 	in aux 1
-	
+
 let counter_one = count_upto 10
