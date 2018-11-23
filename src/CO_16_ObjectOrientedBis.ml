0d15c7e2b94f6d0ec1d03803b59aa2eb6355aa62
Adrien Baudet <baudetad@gmail.com>
2018-11-23 18:00:58 +0100

    Added version

---
+++ src/CO_16_ObjectOrientedBis.ml
@@ -0,0 +1,13 @@
+class counter =
+  object (self)
+    val mutable x = 1
+    method increment = x <- x + 1
+    method print = (string_of_int x) |> print_endline
+    method count_utpo maxi = while x <= maxi do
+      self#print;
+      self#increment;
+    done
+  end;;
+
+let counter_sixteen = new counter;;
+counter_sixteen#count_utpo 10
