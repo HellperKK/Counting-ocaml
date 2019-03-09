2c770bedad7338b6baffb12eb7207ed10616c2fa
Adrien Baudet <baudetad@gmail.com>
2019-03-09 19:00:29 +0100

    small edit

--- src/CO_18_RecursiveTwo.ml
+++ src/CO_18_RecursiveTwo.ml
@@ -3,7 +3,6 @@ let print_int_endline x = (string_of_int x) |> print_endline
 let counter max =
   let rec first x = if x <= max
     then let _ = print_int_endline x in x |> succ |> second
-
   and second x = if x <= max
     then let _ = print_int_endline x in x |> succ |> first
   in first 0
