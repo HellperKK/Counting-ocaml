38a34f4459541d3dcd40fea793ff86f2284b0c73
Adrien Baudet <baudetad@gmail.com>
2019-03-09 18:46:07 +0100

    Small edit

--- src/CO_15_Functor.ml
+++ src/CO_15_Functor.ml
@@ -1,10 +1,11 @@
 module type X_int = sig val x : int end
+
 module Make_counter (X : X_int) =
 struct
   let print_int x = (string_of_int x) |> print_endline
   let count () =
     let rec count_aux i =
-      if i <= X.x then let _ = print_int i in count_aux (i + 1)
+      if i <= X.x then let _ = print_int i in (succ i) |> count_aux
     in count_aux 1
 end
 
