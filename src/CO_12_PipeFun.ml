ea6dcfbee1f8a358fb140b777a1b76ff2a4ad10a
Adrien Baudet <baudetad@gmail.com>
2019-03-09 18:51:21 +0100

    changed (+) 1 with succ

--- src/CO_12_PipeFun.ml
+++ src/CO_12_PipeFun.ml
@@ -2,7 +2,7 @@ let print_int_bis x = let _ = x |> string_of_int |> print_endline in x
 
 let count_piped max =
   let rec aux x = if x <= max
-    then x |> print_int_bis |> ((+) 1) |> aux
+    then x |> print_int_bis |> succ |> aux
   in aux 1
 
 let counter_twelve = count_piped 10
